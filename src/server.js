d981cf29383332fba688b0bda9be393e5c8519ec
{hc456123@gmail.com} <hc456123@gmail.com>
2021-11-23 16:17:24 +0900

    2021-11-23 Add Front Part

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ app.use(session({
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/assets", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
